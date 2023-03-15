862279e61a4406b2067bb90f6325d9f401c853fc
NirmalGundlapelly <nirmalgoud2103@gmail.com>
2023-03-15 15:18:38 +0530

    ui-design & register fun..

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase.js";
 import { useNavigate } from "react-router-dom";
 
@@ -8,6 +12,14 @@ import "./Login.css";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [isRegistered, setRegistered] = useState(false);
+  const [registerInformation, setRegisterInformation] = useState({
+    email: "",
+    confirmEmail: "",
+    password: "",
+    confirmPassword: "",
+  });
 
   const navigate = useNavigate();
 
@@ -28,6 +40,17 @@ export default function Login() {
       .then(() => {
         navigate("/home");
       })
+      .catch((error) => {
+        alert(error.message);
+        setShowError(true);
+      });
+  };
+
+  const onClickRegister = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate("/home");
+      })
       .catch((error) => alert(error.message));
   };
 
@@ -35,17 +58,101 @@ export default function Login() {
     <div className="bg_container">
       <h1 className="todo_heading">Todo Login</h1>
       <div className="form_container">
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" onChange={onChangeEmail} value={email} />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          onChange={onChangePassword}
-          value={password}
-        />
-        <button onClick={onClickSignIn}>Sign In</button>
-        <button>Create an Account</button>
+        {isRegistered ? (
+          <>
+            <input
+              className="input_element"
+              type="email"
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  email: e.target.value,
+                })
+              }
+              value={registerInformation.email}
+              placeholder="Email"
+            />
+            <input
+              className="input_element"
+              type="email"
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  confirmEmail: e.target.value,
+                })
+              }
+              value={registerInformation.confirmEmail}
+              placeholder="Confirm Email"
+            />
+            <input
+              className="input_element"
+              type="password"
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  password: e.target.value,
+                })
+              }
+              value={registerInformation.password}
+              placeholder="Password"
+            />
+            <input
+              className="input_element"
+              type="password"
+              onChange={(e) =>
+                setRegisterInformation({
+                  ...registerInformation,
+                  confirmPassword: e.target.value,
+                })
+              }
+              value={registerInformation.confirmPassword}
+              placeholder="Confirm Password"
+            />
+            <button className="sign_button" onClick={onClickRegister}>
+              Register
+            </button>
+            <button
+              onClick={() => setRegistered(false)}
+              className="create_button"
+            >
+              Go Back
+            </button>
+          </>
+        ) : (
+          <>
+            <label htmlFor="email">Email</label>
+            <input
+              className="input_element"
+              type="text"
+              id="email"
+              onChange={onChangeEmail}
+              value={email}
+              placeholder="Email"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              className="input_element"
+              type="password"
+              id="password"
+              onChange={onChangePassword}
+              value={password}
+              placeholder="Password"
+            />
+            <button className="sign_button" onClick={onClickSignIn}>
+              Sign In
+            </button>
+            <p className="or_text">------- or -------</p>
+            <button
+              onClick={() => setRegistered(true)}
+              className="create_button"
+            >
+              Create an Account
+            </button>
+            {showError && (
+              <p className="error_message">* Invalid Email or Password</p>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
